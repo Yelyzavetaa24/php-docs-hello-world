c0d63c9594b6a28b332e8e33fe9839e8bf25614b
Yelyzavetaa24 <129311144+Yelyzavetaa24@users.noreply.github.com>
2026-01-21 17:08:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-
-echo "Hello World!";
+$studentId = getenv("STUDENT_ID") ?: "BRAK";
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>WebApp 50255</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+  <p>Unikalny komunikat: student 50255</p>
+  <p>STUDENT_ID (z Azure): <strong><?php echo htmlspecialchars($studentId); ?></strong></p>
+</body>
+</html>
